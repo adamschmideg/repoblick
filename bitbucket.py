32eac21d1d93f7dd059bcc82a46322312f5c693a
Adam Schmideg <adam@schmideg.net>
2010-12-21 23:11:06 +0100

    Read commits count for bitbucket

--- bitbucket.py
+++ bitbucket.py
@@ -7,11 +7,14 @@ def list_repos(subpage='all/commits', count=20):
     url = 'https://bitbucket.org/repo/%s/%s' % (subpage, p + 1)
     print 'Opening', url
     page = html.parse(urlopen(url))
-    links = page.xpath("/html/body/div/div/ul[@id='repositories']/li/dl/dd[@class='name']/a[2]/@href")
-    for l in links:
-      if l.startswith('http'):
-        schema, _, domain, user, project, _ = l.split('/')
+    projects = page.xpath("/html/body/div/div/ul[@id='repositories']/li/dl")
+    for prj in projects:
+      link = prj.xpath("dd[@class='name']/a[2]/@href")[0]
+      commits = prj.xpath("dd[@class='commits']/div/span[1]/a/text()")[0]
+      commits = int(commits.replace(',', ''))
+      if link.startswith('http'):
+        schema, _, domain, user, project, _ = link.split('/')
       else:
-        _, user, project, _ = l.split('/')
-      repos.append((user, project))
+        _, user, project, _ = link.split('/')
+      repos.append((user, project, commits))
   return repos
