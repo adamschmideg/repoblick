9d08de3e766bc5fb444409a5d55045ee07e66897
Adam Schmideg <adam@schmideg.net>
2010-12-23 02:37:12 +0100

    clone first couple of commits from bitbucket

--- bitbucket.py
+++ bitbucket.py
@@ -1,11 +1,11 @@
 from lxml import html
 from urllib2 import urlopen
+from subprocess import Popen, PIPE
+import os, re
 
 def list_repos(subpage='all/commits', count=20):
-  repos = []
   for p in range(count / 20):
     url = 'https://bitbucket.org/repo/%s/%s' % (subpage, p + 1)
-    print 'Opening', url
     page = html.parse(urlopen(url))
     projects = page.xpath("/html/body/div/div/ul[@id='repositories']/li/dl")
     for prj in projects:
@@ -16,5 +16,22 @@ def list_repos(subpage='all/commits', count=20):
         schema, _, domain, user, project, _ = link.split('/')
       else:
         _, user, project, _ = link.split('/')
-      repos.append((user, project, commits))
-  return repos
+      yield (user, project, commits)
+
+def repo_inits(user, project, dir='/tmp', count=2):
+  """Get count number of initial changes [(files, lines_added, lines_removed)]"""
+  result = []
+  user_dir = os.path.join(dir, user)
+  if not os.path.exists(user_dir):
+    os.mkdir(user_dir)
+  orig_repo = 'https://bitbucket.org/%s/%s' % (user, project)
+  clone_repo = os.path.join(user_dir, project)
+  if os.path.exists(clone_repo):
+    p = Popen(['hg', 'pull', '-r', str(count - 1), '-R', clone_repo], stdout=PIPE)
+  else:
+    p = Popen(['hg', 'clone', '-r', str(count - 1), orig_repo, clone_repo], stdout=PIPE)
+  p = Popen(['hg', 'log', '-R', clone_repo, '--template', '{diffstat}\n'], stdout=PIPE)
+  for line in p.stdout.readlines():
+    m = re.match('([0-9]+):\s*\+([0-9]+)/\-([0-9]+)', line)
+    result.append([int(n) for n in m.groups()])
+  return result
