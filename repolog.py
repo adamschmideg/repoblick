8b68cce4e8a58cce828950f4851a53ecc20afe09
Adam Schmideg <adam@schmideg.net>
2010-12-28 15:33:39 +0100

    Tables have their own primary key id

--- repolog.py
+++ repolog.py
@@ -8,11 +8,12 @@ class SqliteStore:
 
   def __init__(self, path):
     self.path = path
-    self.connection = sqlite3.connect(path)
+    self.cursor = sqlite3.connect(path).cursor()
     tables = (
       '''
       projects (
-        project text primary key,
+        id integer primary key autoincrement,
+        project text,
         host text,
         commits integer,
         watchers integer,
@@ -22,67 +23,68 @@ class SqliteStore:
       )''',
       '''
       commits (
+        id integer primary key autoincrement,
         author text,
         date timestamp,
-        hash text primary key,
-        project text,
-        foreign key(project) references projects(project)
+        hash text,
+        projectid integer,
+        foreign key(projectid) references projects(id)
       )''',
       '''
       files (
-        commithash text,
+        id integer primary key autoincrement,
+        commitid integer,
         changetype char(1),
         added integer,
         deleted integer,
         file text,
         isbinary boolean,
-        foreign key(commithash) references commits(hash)
+        foreign key(commitid) references commits(id)
       )''',
     )
     for t in tables:
       try:
-        self.connection.execute('create table if not exists %s;' % t)
+        self.cursor.execute('create table if not exists %s;' % t)
       except sqlite3.OperationalError, e:
         print e, t
       
   def project(self, mirror):
-    self.connection.execute('''
+    self.cursor.execute('''
       insert into projects (project, host, commits, watchers, forks, language)
       values(:project, :remoteRoot, :commitCount, :watchers, :forks, :language)''',
       mirror.__dict__)
+    return self.cursor.lastrowid
 
-  def commit(self, commit):
-    self.connection.execute('''
-      insert into commits(author, date, hash, project)
-      values(:author, :date, :hash, :project)''',
-      commit.__dict__)
-    return hash
+  def commit(self, projectid, commit):
+    self.cursor.execute('''
+      insert into commits(author, date, hash, projectid)
+      values(:author, :date, :hash, :projectid)''',
+      dict(commit.__dict__, projectid=projectid))
+    return self.cursor.lastrowid
 
-  def file(self, fileChange):
-    self.connection.execute('''
-      insert into files(file, commithash, changetype, added, deleted, isbinary)
-      values(:filename, :commitHash, :changeType, :addedLines, :deletedLines, :isBinary)''',
-      fileChange.__dict__)
+  def file(self, commitid, fileChange):
+    self.cursor.execute('''
+      insert into files(file, commitid, changetype, added, deleted, isbinary)
+      values(:filename, :commitid, :changeType, :addedLines, :deletedLines, :isBinary)''',
+      dict(fileChange.__dict__, commitid=commitid))
+    return self.cursor.lastrowid
     
   def importLog(self, mirror):
     "Import logs of a repoMirror into this store"
     with Timer('Import %s' % mirror.project):
       try:
-        self.project(mirror)
+        projectid = self.project(mirror)
       except sqlite3.IntegrityError:
         # project already exists, delete its logs
-        self.connection.execute('''
+        self.cursor.execute('''
           delete from files where commithash in
             (select hash from commits where project=?)''',
           (mirror.project,))
-        self.connection.execute('delete from commits where project=?',
+        self.cursor.execute('delete from commits where project=?',
           (mirror.project,))
-      cnt = 0
       for commit in mirror.commits():
-        cnt += 1
-        commit.project = mirror.project
-        self.commit(commit)
+        commitid = self.commit(projectid, commit)
         for fileChange in mirror.changes(commit):
-          self.file(fileChange)
-      self.connection.commit()
+          self.file(commitid, fileChange)
+      self.cursor.connection.commit()
     print 'Storage used:\t%s' % fileSize(self.path)
